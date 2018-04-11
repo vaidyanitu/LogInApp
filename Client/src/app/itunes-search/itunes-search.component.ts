d44366805b2b440d4a263374eba1df61868cd31a
nitu.vaidya <nitu.vaidya@softvision.com>
2018-04-11 12:50:20 +0545

    implements datatable destroy for itunessearch

--- Client/src/app/itunes-search/itunes-search.component.ts
+++ Client/src/app/itunes-search/itunes-search.component.ts
@@ -9,58 +9,69 @@ import {DataTableDirective} from 'angular-datatables';
   styleUrls: ['./itunes-search.component.css']
 })
 export class ItunesSearchComponent implements OnInit, AfterViewInit {
-loading:boolean=false;
-@ViewChild(DataTableDirective) 
-dtElement:DataTableDirective;
-results=[];
-dtOptions: DataTables.Settings = {};
-dtTrigger: Subject<any> = new Subject();
+  loading: boolean = false;
+  @ViewChild(DataTableDirective)
+  dtElement: DataTableDirective;
+  results = [];
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<any> = new Subject();
 
-  constructor(private itunes:SearchService) { 
-    this.loading=false;
+  constructor(private itunes: SearchService) {
+    this.loading = false;
   }
 
   ngOnInit() {
     // this.results[0]={name:"test",artist:"test",thumbnail:"Test",artistId:"test",link:"test"};
     this.dtOptions = {
       pagingType: 'full_numbers',
-      pageLength:4,      
+      pageLength: 4,
     };
   }
 
-  ngAfterViewInit(){
+  ngAfterViewInit() {
     this.dtTrigger.next();
   }
 
-  doSearch(term:string){
+  doSearch(term: string) {
     debugger;
-    this.loading=true;
+    this.loading = true;
+    this.rerender();
     // console.log(this.dtElement);
     // this.dtElement.dtInstance.then((dtInstance:DataTables.Api)=>{
     //   //Destroy the table first
     //   dtInstance.destroy();
     //   //switch
-      this.itunes.search(term)
-                  .subscribe(x=>{
-                    console.log(x.json());
-                    this.loading=true;
-                    this.results=x.json().results.map(item=>{
-                      return new SearchItem(
-                        item.trackName,
-                        item.artistName,
-                        item.trackViewUrl,
-                        item.artworkUrl30,
-                        item.artistId
-                        );
-                  });
-                  console.log(this.results);
-                  this.dtTrigger.next();
-                } ,
-                err=>console.log("Error:",err));  
+    this.itunes.search(term)
+      .subscribe(x => {
+        console.log(x.json());
+        this.loading = true;
+        this.results = x.json().results.map(item => {
+          return new SearchItem(
+            item.trackName,
+            item.artistName,
+            item.trackViewUrl,
+            item.artworkUrl30,
+            item.artistId
+          );
+        });
+        console.log(this.results);
+        this.dtTrigger.next();
+      },
+      err => console.log("Error:", err));
     // })  
     
-   }
-  
+  }
+
+  rerender(): void {
+    if (this.dtElement != undefined) {
+      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+        dtInstance.destroy();
+        //this.dtTrigger.next();
+      });
+    }  
+  }
+
+
 }
 
 class SearchItem{
--- Client/src/app/youtube-search/youtube-search.component.ts
+++ Client/src/app/youtube-search/youtube-search.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { SearchResult } from '../youtube-search/searchresult';
+
+import { DataTableDirective } from 'angular-datatables';
 
 import{Http,Response} from '@angular/http';
 import { Subject } from 'rxjs/Subject';
@@ -9,10 +11,12 @@ import 'rxjs/add/operator/map';
   selector: 'youtube-search',
   templateUrl: './youtube-search.component.html' 
 })
-export class YouTubeSearchComponent implements OnInit {
+export class YouTubeSearchComponent implements OnInit, AfterViewInit {
 results:SearchResult[];
 loading:boolean=false;
 
+  @ViewChild(DataTableDirective)
+  dtElement: DataTableDirective;
 dtOptions:DataTables.Settings={};
 dtTrigger:Subject<any>=new Subject();
 
@@ -25,9 +29,24 @@ dtTrigger:Subject<any>=new Subject();
     };
   }
 
-  updateResults(results:SearchResult[]):void{
+  ngAfterViewInit() {
+    this.dtTrigger.next();
+  }
+  
+  updateResults(results: SearchResult[]): void{
+    this.rerender();
     this.results=results; 
     this.dtTrigger.next(); 
     this.loading=true; 
   }
+
+  rerender(): void {
+    if (this.dtElement != undefined) {
+      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+        dtInstance.destroy();
+        //this.dtTrigger.next();
+      });
+    }
+  }
+
 }
